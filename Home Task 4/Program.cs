d56ba86748cc948a4e11a4324b386a26d1c61770
Əli Cabbarov <elicabbarov88@gmail.com>
2024-10-20 01:48:00 -0700

    New Home Task

---
+++ Home Task 4/Program.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections.Immutable;
+using System.Globalization;
+
+namespace Home_Task_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // 1.Verilmiş ededler siyahisindaki(int[]) en böyük ededi tapan alqoritm
+
+            //int[] imputNumbers = new int[] { 20, 30, 60, 40, 200, 250, 300, 350, 34, 64 };
+            //int number1 = 0;
+            //int BiggestNumber = 0;
+            //for (int i = 0; i < imputNumbers.Length; i++)
+            //{
+            //    BiggestNumber = imputNumbers.Max();
+                
+            //}
+            //Console.WriteLine(BiggestNumber);
+
+
+            //// 2.Verilmiş Products arrayındakı Product objectlərinin Id'ləri tək olanların ədədi ortasını tapan alqoritminin obyektlərinin xüsusiyyətləri: Id(yeni reqem ve ya eded), Name, Price, StockCount )
+            //var Product1 = new { ID = 1, Name = "Su", Price = 20, Stock = 1000 };
+            //var Product2 = new { ID = 2, Name = "Cola", Price = 25, Stock = 1000 };
+            //var Product3 = new { ID = 3, Name = "Sprite", Price = 23, Stock = 1000 };
+            //var Product4 = new { ID = 4, Name = "Cay", Price = 22, Stock = 1000 };
+            //var Product5 = new { ID = 5, Name = "Coffe", Price = 26, Stock = 1000 };
+            //var products = new[] { Product1, Product2, Product3, Product4, Product5 };
+            //var count = 0;
+            //var sum = 0;
+            //    for (int i = 0; i < products.Length; i++)
+            //    {
+            //        if (products[i].ID % 2 != 0) 
+            //        {
+            //            sum += products[i].ID; 
+            //            count++; 
+            //        }
+            //    }
+            //      int average = sum / count;
+                   
+            //Console.WriteLine($"Tek ID'lerin ortalaması: {average}"); 
+               
+            
+
+
+
+
+            
+        }
+    }
+}
